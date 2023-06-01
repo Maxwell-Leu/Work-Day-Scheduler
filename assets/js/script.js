c7952be22f9137951426ea50669101265ee900d7
Maxwell L <mleuthner2002@gmail.com>
2023-06-01 11:26:19 -0500

    Added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 $(function () {
+  // List of all of the ids for the hours of the work day
   var hourIds = ["#hour-9", "#hour-10", "#hour-11", "#hour-12", "#hour-13", "#hour-14", "#hour-15", "#hour-16", "#hour-17"];
 
+  // Targets all save buttons
   var saveButton = $('.saveBtn');
   
+  // Saves inputed text in local storage. Objects are named after the hour of the work day.
   function saveEvent(event){
     event.preventDefault();
     var key = $(this).parent().attr('id')
@@ -14,10 +17,13 @@ $(function () {
     localStorage.setItem(key, JSON.stringify(eventInfo));
   }
 
+  // Buttons run save event on click
   saveButton.on('click', saveEvent);
 
+  // Gets the current hour of the day on page load
   var hourOfDay = dayjs().get('hour');
 
+  // Checks to see which time slots need to be changed off of their defualt state
   for(i = 0; i < hourIds.length; i++){
     var changeToPast = document.querySelector(hourIds[i]);
 
@@ -27,7 +33,7 @@ $(function () {
         changeToPast.classList.add('present');
       }
     }
-    
+
     if(hourOfDay > hourIds[i].split("-")[1]){
       
       if (changeToPast.classList.contains('present')){
@@ -40,6 +46,7 @@ $(function () {
     }
   }
   
+  // Parses through local storage to display saved events
   for(i = 0; i < localStorage.length; i++){
     var eventInfo = JSON.parse(localStorage.getItem(localStorage.key(i)));
     var id = "#"+eventInfo.eventId
@@ -47,6 +54,7 @@ $(function () {
     console.log(id)
   }
 
+  // Shows current date
   var date = dayjs().format('dddd, MMMM D');
   $('#currentDay').text(date);
 });
